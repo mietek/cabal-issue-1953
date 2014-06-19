4c0d3f773621107ee3eeac90629ea8587853f26b
Miëtek Bak <mietek@bak.io>
2014-06-19 11:58:20 +0100

    Expect 42 as an arg for test

--- test/main.hs
+++ test/main.hs
@@ -1,10 +1,12 @@
 module Main (main) where
 
 import Foobar (foobar)
+import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
 
 main :: IO ()
-main =
-    case foobar of
-      42 -> exitSuccess
+main = do
+    args <- getArgs
+    case (args, foobar) of
+      (["42"], 42) -> exitSuccess
       _ -> exitFailure
